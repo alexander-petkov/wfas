58936fa65c63f288b60485b432ef44df28ce0939
Alexander Petkov <greenkov@gmail.com>
2019-06-26 13:50:47 +0000

    Initial version of an external bash script for RTMA ftp archive sync and imagemosaic coverage management.

---
+++ rtma-mosaic/bin/del_rtma.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+#RTMA grib files local storage locations:
+FILE_DIR='/mnt/cephfs/wfas/data/rtma/met/grib/'
+
+#Sync to the most current RTMA FTP archive: 
+wget --cut-dirs 6 -xnH -c --recursive --directory-prefix=$FILE_DIR -N  --no-parent -Artma2p5.*.2dvaranl_ndfd.grb2_wexp ftp://ftp.ncep.noaa.gov/pub/data/nccf/com/rtma/prod/rtma2p5.*
+
+#Get a list of RTMA coverages:
+COVERAGES=(`curl -s -u admin:geoserver -XGET http://192.168.59.56:8081/geoserver/rest/workspaces/landfire/coveragestores/rtma_mosaic/coverages.xml |grep -oP '(?<=<name>).*?(?=</name>)'`)
+
+#List of locally stored Grib files:
+CUR_FILES=(`find ${FILE_DIR} -name '*.2dvaranl_ndfd.grb2_wexp' |sort`)
+
+#List of Mosaic granules:
+MOSAIC_FILES=`curl -s -u admin:geoserver -XGET http://192.168.59.56:8081/geoserver/rest/workspaces/landfire/coveragestores/rtma_mosaic/coverages/${COVERAGES[0]}/index/granules.xml |grep -oP '(?<=<gf:location>).*?(?=</gf:location>)'`
+
+for c in ${CUR_FILES[@]}
+do 
+ found=0
+ for m in ${MOSAIC_FILES[@]}
+ do
+   if [ "$c" = "$m" ]; then
+     echo "Granule already in mosaic:" $c
+     found=1 
+     break
+   fi
+ done
+ if [ $found = 0 ]; then 
+   #add granule to mosaic:
+   echo "Adding granule:" $c
+   curl -v -u admin:geoserver -XPOST -H "Content-type: text/plain" -d "file:${c}" http://192.168.59.56:8081/geoserver/rest/workspaces/landfire/coveragestores/rtma_mosaic/external.imagemosaic
+ fi
+done    
+
+function remove_file_from_mosaic {
+   for c in ${COVERAGES[@]}
+   do
+	#get granule id	for this file:
+	gran_id=`curl -s -uadmin:geoserver -XGET http://192.168.59.56:8081/geoserver/rest/workspaces/landfire/coveragestores/rtma_mosaic/coverages/${c}/index/granules.xml?filter=location=%27${1}%27|grep -oP '(?<=<gf:'${c}' fid=").*?(?=">)'`
+        echo "Should delete granule:" ${gran_id}
+	#delete the granule, using the fid retrieved above:
+	curl -s -u admin:geoserver -XDELETE "http://192.168.59.56:8081/geoserver/rest/workspaces/landfire/coveragestores/rtma_mosaic/coverages/${c}/index/granules/${gran_id}"
+   done
+}
+
+
+
+echo "Before delete:" ${#CUR_FILES[@]}
+ 
+
+for i in ${CUR_FILES[@]}
+do 
+  f=`echo ${i}|cut -d '/' -f 9-`
+  if ! curl -I ftp://ftp.ncep.noaa.gov/pub/data/nccf/com/rtma/prod/$f; then
+    #remove old granules from the mosaic:
+    remove_file_from_mosaic $i
+    #remove old granule from system:
+    subdir=`echo ${i}|cut -d '/' -f 9`
+    to_delete=`echo ${i}|cut -d '/' -f 10` #extract filename
+    rm -rf $FILE_DIR/$subdir/*${to_delete}*
+  else
+    break
+  fi;
+done
+
+CUR_FILES=(`find ${FILE_DIR} -name '*.2dvaranl_ndfd.grb2_wexp'|sort`)
+
+echo "After delete:" ${#CUR_FILES[@]}
+#Remove empty directories:
+find ${FILE_DIR} -empty -type d -name 'rtma2p5.*' -delete
+
