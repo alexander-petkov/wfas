e947d644b608d30a094148276223e251dc937fc1
Alexander Petkov <greenkov@gmail.com>
2019-07-25 19:27:15 +0000

    Added the PCP variable to the archive update script.

--- rtma-mosaic/bin/update_rtma.sh
+++ rtma-mosaic/bin/update_rtma.sh
@@ -2,43 +2,28 @@
 
 REST_URL="http://192.168.59.56:8081/geoserver/rest/workspaces"
 WORKSPACE="rtma"
-RTMA_MOSAIC="rtma_mosaic"
+DATASETS=('varanl' 'pcp')
+PATTERNS=('rtma2p5.*.2dvaranl_ndfd.grb2_wexp' 'rtma2p5.*.pcp.184.grb2')
+counter=0
 
-#RTMA grib files local storage locations:
-FILE_DIR='/mnt/cephfs/wfas/data/rtma/met/grib/'
-
-#Sync to the most current RTMA FTP archive: 
-wget --cut-dirs 6 -xnH -c --recursive --directory-prefix=$FILE_DIR -N  --no-parent \
-	-Artma2p5.*.2dvaranl_ndfd.grb2_wexp \
-	ftp://ftp.ncep.noaa.gov/pub/data/nccf/com/rtma/prod/rtma2p5.*
-
-#Get a list of RTMA coverages:
-COVERAGES=(`curl -s -u admin:geoserver -XGET ${REST_URL}/${WORKSPACE}/coveragestores/${RTMA_MOSAIC}/coverages.xml \
-		|grep -oP '(?<=<name>).*?(?=</name>)'`)
-
-#List of locally stored Grib files:
-CUR_FILES=(`find ${FILE_DIR} -name '*.2dvaranl_ndfd.grb2_wexp' |sort`)
-
-#List of Mosaic granules:
-MOSAIC_FILES=`curl -s -u admin:geoserver -XGET ${REST_URL}/${WORKSPACE}/coveragestores/${RTMA_MOSAIC}/coverages/${COVERAGES[0]}/index/granules.xml |grep -oP '(?<=<gf:location>).*?(?=</gf:location>)'`
-
+function add_new_files_to_mosaic {
 for c in ${CUR_FILES[@]}
-do 
- found=0
- for m in ${MOSAIC_FILES[@]}
- do
-   if [ "$c" = "$m" ]; then
-     echo "Granule already in mosaic:" $c
-     found=1 
-     break
+do
+   found=0
+   for m in ${MOSAIC_FILES[@]}
+   do
+      if [ "$c" = "$m" ]; then
+         found=1 
+         break
+      fi
+   done
+   if [ $found = 0 ]; then 
+      #add granule to mosaic:
+      echo "Adding granule:" $c
+      curl -s -u admin:geoserver -XPOST -H "Content-type: text/plain" -d "file://"$c "${REST_URL}/${WORKSPACE}/coveragestores/${RTMA_MOSAIC}/external.imagemosaic"
    fi
- done
- if [ $found = 0 ]; then 
-   #add granule to mosaic:
-   echo "Adding granule:" $c
-   curl -s -u admin:geoserver -XPOST -H "Content-type: text/plain" -d "file://"${c} "${REST_URL}/${WORKSPACE}/coveragestores/${RTMA_MOSAIC}/external.imagemosaic"
- fi
-done    
+done
+}
 
 function remove_file_from_mosaic {
    for c in ${COVERAGES[@]}
@@ -50,23 +35,46 @@ function remove_file_from_mosaic {
    done
 }
 
-for i in ${CUR_FILES[@]}
+#loop over RTMA datasets:
+for d in ${DATASETS[@]}
 do 
-  f=`echo ${i}|cut -d '/' -f 9-`
-  if ! curl -I ftp://ftp.ncep.noaa.gov/pub/data/nccf/com/rtma/prod/$f; then
-    #remove old granules from the mosaic:
-    echo "removing" ${i}, ${f}
-    remove_file_from_mosaic $i
-    #remove old granule from system:
-    subdir=`echo ${i}|cut -d '/' -f 9`
-    to_delete=`echo ${i}|cut -d '/' -f 10` #extract filename
-    rm -rf $FILE_DIR/$subdir/*${to_delete}*
-    rm -rf $FILE_DIR/$subdir/.${to_delete}*
-  else
-    break
-  fi;
+   RTMA_MOSAIC="rtma_"${d}
+   #RTMA grib files local storage locations:
+   FILE_DIR='/mnt/cephfs/wfas/data/rtma/'${d}
+
+   #Sync to the most current RTMA FTP archive: 
+   wget --cut-dirs 6 -xnH -c --recursive --directory-prefix=$FILE_DIR -N  --no-parent \
+	-A${PATTERNS[counter]} \
+	ftp://ftp.ncep.noaa.gov/pub/data/nccf/com/rtma/prod/rtma2p5.*
+
+   #Get a list of coverages for this mosaic:
+   COVERAGES=(`curl -s -u admin:geoserver -XGET ${REST_URL}/${WORKSPACE}/coveragestores/${RTMA_MOSAIC}/coverages.xml \
+		|grep -oP '(?<=<name>).*?(?=</name>)'`)
+   #List of locally stored Grib files:
+   CUR_FILES=(`find ${FILE_DIR} -name ${PATTERNS[counter]} |sort`)
+
+   #List of Mosaic granules:
+   MOSAIC_FILES=`curl -s -u admin:geoserver -XGET ${REST_URL}/${WORKSPACE}/coveragestores/${RTMA_MOSAIC}/coverages/${COVERAGES[0]}/index/granules.xml |grep -oP '(?<=<gf:location>).*?(?=</gf:location>)'`
+
+   add_new_files_to_mosaic 
+
+   for i in ${CUR_FILES[@]}
+   do 
+      f=`echo ${i}|cut -d '/' -f 8-`
+      if ! curl -I ftp://ftp.ncep.noaa.gov/pub/data/nccf/com/rtma/prod/$f; then
+         #remove old granules from the mosaic:
+         remove_file_from_mosaic $i
+         #remove old granule from system:
+         subdir=`echo ${i}|cut -d '/' -f 8`
+         to_delete=`echo ${i}|cut -d '/' -f 9` #extract filename
+         rm -rf $FILE_DIR/$subdir/*${to_delete}*
+         rm -rf $FILE_DIR/$subdir/.${to_delete}*
+      else
+         break;
+      fi;
+   done
+
+   #Remove empty directories:
+   find ${FILE_DIR} -empty -type d -name 'rtma2p5.*' -delete
+   (( counter++ ))
 done
-
-#Remove empty directories:
-find ${FILE_DIR} -empty -type d -name 'rtma2p5.*' -delete
-
