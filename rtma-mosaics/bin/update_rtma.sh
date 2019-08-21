eba0750fa7f7d6738ca2564e4a130f2ebe07226d
Alexander Petkov <greenkov@gmail.com>
2019-08-21 04:05:18 +0000

    Added rhm dataset to the RTMA update script.

--- rtma-mosaics/bin/update_rtma.sh
+++ rtma-mosaics/bin/update_rtma.sh
@@ -2,10 +2,32 @@
 
 REST_URL="http://192.168.59.56:8081/geoserver/rest/workspaces"
 WORKSPACE="rtma"
-DATASETS=('varanl' 'pcp')
-PATTERNS=('rtma2p5.*.2dvaranl_ndfd.grb2_wexp' 'rtma2p5.*.pcp.184.grb2')
+RTMA_DIR='/mnt/cephfs/wfas/data/rtma'
+DATASETS=('varanl' 'pcp' 'rhm')
+PATTERNS=('rtma2p5.*.2dvaranl_ndfd.grb2_wexp' 'rtma2p5.*.pcp.184.grb2' 'rtma2p5.*.2dvaranl_ndfd.grb2_wexp')
+#DATASETS=('rhm')
+#PATTERNS=('rtma2p5.*.2dvaranl_ndfd.grb2_wexp')
 counter=0
 
+#cdo-related setup:
+export LD_LIBRARY_PATH=/mnt/cephfs/wfas/bin/eccodes-2.12.5/build/lib
+export ECCODES_DEFINITION_PATH=/mnt/cephfs/wfas/bin/eccodes-2.12.5/build/share/eccodes/definitions
+export ECCODES_SAMPLES_PATH=/mnt/cephfs/wfas/bin/eccodes-2.12.5/build/share/eccodes/samples
+
+function derive_rhm {
+   for src in `find ${RTMA_DIR}/varanl -name '*_wexp' |sort`
+   do
+     f=`echo ${src}|cut -d '/' -f 8-`
+     if [ ! -f ${RTMA_DIR}/rhm/${f} ]; then
+       dirname=`echo ${src}|cut -d '/' -f 8`
+       mkdir -p ${RTMA_DIR}/rhm/${dirname}      
+       /mnt/cephfs/wfas/bin/cdo-1.9.7.1/src/cdo \
+		-expr,'2r=(exp(1.81+(2d*17.27- 4717.31) / (2d - 35.86))/exp(1.81+(2t*17.27- 4717.31) / (2t - 35.86)))*100' \
+		${src} ${RTMA_DIR}/rhm/$f
+     fi
+   done
+}		      
+	
 function add_new_files_to_mosaic {
 for c in ${CUR_FILES[@]}
 do
@@ -17,7 +39,7 @@ do
          break
       fi
    done
-   if [ $found = 0 ]; then 
+   if [ $found = 0 ]; then
       #add granule to mosaic:
       echo "Adding granule:" $c
       curl -s -u admin:geoserver -XPOST -H "Content-type: text/plain" -d "file://"$c "${REST_URL}/${WORKSPACE}/coveragestores/${RTMA_MOSAIC}/external.imagemosaic"
@@ -40,12 +62,18 @@ for d in ${DATASETS[@]}
 do 
    RTMA_MOSAIC="rtma_"${d}
    #RTMA grib files local storage locations:
-   FILE_DIR='/mnt/cephfs/wfas/data/rtma/'${d}
+   FILE_DIR=${RTMA_DIR}/${d}
 
-   #Sync to the most current RTMA FTP archive: 
-   wget -q --cut-dirs 6 -xnH -c --recursive --directory-prefix=$FILE_DIR -N  --no-parent \
-	-A${PATTERNS[counter]} \
-	ftp://ftp.ncep.noaa.gov/pub/data/nccf/com/rtma/prod/rtma2p5.*
+   #Sync to the most current RTMA FTP archive
+   #Unless we have the Rel Humidity (rhm) dataset, 
+   #which is derived from varanl data:
+   if [ ! ${d} = 'rhm' ] ; then
+      wget -q --cut-dirs 6 -xnH -c --recursive --directory-prefix=$FILE_DIR -N  --no-parent \
+	   -A${PATTERNS[counter]} \
+	   ftp://ftp.ncep.noaa.gov/pub/data/nccf/com/rtma/prod/rtma2p5.*
+   else
+      derive_rhm
+   fi
 
    #Get a list of coverages for this mosaic:
    COVERAGES=(`curl -s -u admin:geoserver -XGET ${REST_URL}/${WORKSPACE}/coveragestores/${RTMA_MOSAIC}/coverages.xml \
@@ -60,7 +88,7 @@ do
 
    for i in ${MOSAIC_FILES[@]}
    do 
-      echo $f
+      f=`echo ${i}|cut -d '/' -f 8-` #get last two tokens, containing dir and file name
       if ! curl -I ftp://ftp.ncep.noaa.gov/pub/data/nccf/com/rtma/prod/$f; then
          #remove old granules from the mosaic:
 	 remove_file_from_mosaic $i
