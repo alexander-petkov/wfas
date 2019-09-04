72d32afc42b52fa765d408bf414067a4ccb77bbe
Alexander Petkov <greenkov@gmail.com>
2019-09-04 20:55:37 +0000

    Changed GFS update script to download 1hr 0.25 deg resolution data.

--- gfs-mosaics/bin/update_gfs.sh
+++ gfs-mosaics/bin/update_gfs.sh
@@ -17,24 +17,25 @@ export ECCODES_SAMPLES_PATH=/mnt/cephfs/wfas/bin/eccodes-2.12.5/build/share/ecco
 CDO_DIR='/mnt/cephfs/wfas/bin/cdo-1.9.7.1/src'
 
 #NOMADS setup:
-RES='0p50' #half-deegree resolution
-NOMADS_URL="https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_${RES}.pl"
-SUBREGION='subregion=&leftlon=220&rightlon=290.84&toplat=53&bottomlat=22'
+RES='0p25' #quarter-degree resolution
+NOMADS_URL="https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_${RES}_1hr.pl"
+HOUR='t00z' #forecast hour
+SUBREGION='subregion=&leftlon=220&rightlon=300&toplat=53&bottomlat=22'
 #get latest forecast run:
 FORECAST=`curl -s -l https://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/|cut -d '"' -f 2|cut -d '/' -f 1|grep 'gfs\.'|tail -n 1`
 #END NOMADS Setup
 
 function derive_wdir {
-   for h in `seq -w 003 3 384`
+   for h in `seq -w 003 1 384`
    do 
-	${CDO_DIR}/cdo -O mulc,57.3 -atan2 -mulc,-1 ${GFS_DIR}/UGRD/gfs.t00z.pgrb2full.${RES}.f${h}.grb2 -mulc,-1 ${GFS_DIR}/VGRD/gfs.t00z.pgrb2full.${res}.f${h}.grb2 ${GFS_DIR}/WDIR/gfs.t00z.pgrb2full.${RES}.f${h}.grb2
+	${CDO_DIR}/cdo -O -b 10 expr,'10wdir=10u' -mulc,57.3 -atan2 -mulc,-1 ${GFS_DIR}/UGRD/gfs.${HOUR}.pgrb2.${RES}.f${h}.grb2 -mulc,-1 ${GFS_DIR}/VGRD/gfs.${HOUR}.pgrb2.${RES}.f${h}.grb2 ${GFS_DIR}/WDIR/gfs.${HOUR}.pgrb2.${RES}.f${h}.grb2
    done
 }
 function derive_wspd {
-   for h in `seq -w 003 3 384`
+   for h in `seq -w 003 1 384`
    do 
-      ${CDO_DIR}/cdo -O select,name=10u,10v ${GFS_DIR}/UGRD/gfs.t00z.pgrb2full.${RES}.f${h}.grb2 ${GFS_DIR}/VGRD/gfs.t00z.pgrb2full.${RES}.f${h}.grb2 ${GFS_DIR}/out.grb2;
-      ${CDO_DIR}/cdo -O expr,'10si=(sqrt(10u*10u+10v*10v))' ${GFS_DIR}/out.grb2 ${GFS_DIR}/WSPD/gfs.t00z.pgrb2full.${RES}.f${h}.grb2
+      ${CDO_DIR}/cdo -O select,name=10u,10v ${GFS_DIR}/UGRD/gfs.${HOUR}.pgrb2.${RES}.f${h}.grb2 ${GFS_DIR}/VGRD/gfs.${HOUR}.pgrb2.${RES}.f${h}.grb2 ${GFS_DIR}/out.grb2;
+      ${CDO_DIR}/cdo -O expr,'10si=(sqrt(10u*10u+10v*10v))' ${GFS_DIR}/out.grb2 ${GFS_DIR}/WSPD/gfs.${HOUR}.pgrb2.${RES}.f${h}.grb2
    done
    rm -f ${GFS_DIR}/out.grb2
 }
@@ -50,10 +51,11 @@ do
    #in which case we calculate it:
 #1. If dataset is not derived, get data
    if [ ${DERIVED[$counter]} = 0 ]; then 
-      for h in `seq -w 003 3 384`
+      for h in `seq -w 003 1 384`
       do 
-         wget -q "${NOMADS_URL}?file=gfs.t00z.pgrb2full.${RES}.f${h}&lev_${LEVEL[$counter]}=on&var_${d}=on&${SUBREGION}&dir=%2F${FORECAST}%2F00" \
-		-O ${FILE_DIR}/gfs.t00z.pgrb2full.${RES}.f${h}.grb2;
+         #wget  "${NOMADS_URL}?file=gfs.${HOUR}.pgrb2.${RES}.f${h}&lev_${LEVEL[$counter]}=on&var_${d}=on&${SUBREGION}&dir=%2F${FORECAST}%2F00" \
+		#-O ${FILE_DIR}/gfs.${HOUR}.pgrb2.${RES}.f${h}.grb2;
+	 find ${FILE_DIR} -empty -delete ;
       done
    elif [ ${DERIVED[$counter]} = 1 ]; then #derive dataset:
       ${FUNCTION[counter]} #execute corresponding derive function
