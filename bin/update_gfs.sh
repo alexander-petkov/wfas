4475cf9b2e3b302a9117b9900c104cddf372f1ab
alexander-petkov <greenkov@gmail.com>
2021-10-04 09:06:54 +0000

    Changed update_gfs.sh to use global extent.

--- bin/update_gfs.sh
+++ bin/update_gfs.sh
@@ -7,10 +7,11 @@ source ${DIR}/gfs_common.sh
 WORKSPACE="gfs"
 GFS_DIR=${DATA_DIR}/gfs
 ELEV_FILE=${GFS_DIR}/gfs_dem.tif
+ELEV_FILE=${GFS_DIR}/aster_dem.tif
 #CONUS extent:
 SUBREGION='subregion=&leftlon=-140&rightlon=-60&toplat=53&bottomlat=22'
+SUBREGION='subregion=&leftlon=-180&rightlon=180&toplat=90&bottomlat=-90'
 
 download_data
-
 update_geoserver
 
