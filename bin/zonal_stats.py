f4696d2ac992da604d494c1d6dbe1c3914737eb9
EC2 Default User <ec2-user@ip-172-31-27-238.us-west-2.compute.internal>
2022-08-24 12:12:05 +0000

    Zonal statistics for Elevation, Landorm and Slope.

---
+++ bin/zonal_stats.py
@@ -0,0 +1,129 @@
+#!/mnt/cephfs/miniconda3/bin/python3
+#import cgitb; cgitb.enable()
+import numpy as np
+import gdal
+import osr
+import fiona
+from fiona import transform
+from pprint import pprint
+from shapely.geometry import shape, Polygon, Point
+from rasterstats import zonal_stats, point_query
+import readline
+import json 
+import cgi
+
+def str2bool(v):
+  return v.lower() in ("yes", "true", "t", "1")
+
+def myconverter(obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        elif isinstance(obj, datetime.datetime):
+            return obj.__str__()
+
+def to_dict (keys,values,bin_edges,percent):
+    #counts, vals = ndarray
+    res = {}
+    if (bin_edges is not None):
+        for i in range(1,len(bin_edges)) :
+            if (bin_edges[i]-1==bin_edges[i-1]):
+                key = str(bin_edges[i-1])
+                bool_mask = (keys==bin_edges[i-1])
+            else:
+                if (i<len(bin_edges)-1):
+                    key = str(bin_edges[i-1]) + '-' + str(bin_edges[i]-1)
+                    bool_mask = (keys>=bin_edges[i-1]) & (keys<=bin_edges[i]-1)
+                else:
+                    key = str(bin_edges[i-1]) + '-' + str(bin_edges[i])
+                    bool_mask = (keys>=bin_edges[i-1]) & (keys<=bin_edges[i])
+
+            value = int(sum(values[bool_mask]))
+            if (bool(percent)):
+                #value= round(value/sum(values),2)
+                value= round((value/sum(values))*100,2)
+            if (value>0):
+                res[key] = value
+            
+    else: #return whole histogram as a dictionary, without binning
+        for i in range(len(keys)) :
+            key = str(keys[i])
+            value = values[i]
+            if (bool(percent)):
+                #value= round(value/sum(values),2)
+                value= round((value/sum(values))*100,2)
+            res[key] = value
+    
+    return res
+
+rasters = {'Elevation': '/data/landfire/CONUS/US_DEM_2016_02192019/Grid/us_dem_2016_tiled.tiff',
+        'Landform' : '/data/ergeo/USA_Landform_30m_WMAS.tif',
+        'Slope' : '/data/ergeo/US_SLOPE_PERCENT_16bit.tif'
+        }
+#1. parse cgi arguments:
+form = cgi.FieldStorage()
+bins = None
+percent = False
+
+#get url path for input json
+if 'zoneurl' in form:
+    zoneurl=form.getvalue('zoneurl')
+else:
+   print("Argument zoneurl is required for this script to run, exiting...")
+
+if 'bins' in form:
+    bins = int(form.getvalue('bins'))
+
+if 'percent' in form:
+    percent=str2bool(form.getvalue('percent'))
+
+c= fiona.open(zoneurl,'r')
+
+results=[]
+
+for zone in c.items():
+    lon = zone[1]['properties']['longitude']
+    lat = zone[1]['properties']['latitude']
+    keys = list(rasters.keys())
+    for k in range(len(keys)):
+        r= gdal.Open(rasters[keys[k]])
+        raster_proj = osr.SpatialReference(wkt=r.GetProjection())
+        if ( isinstance(lon ,float) and
+            isinstance(lat ,float)) : 
+            pt = transform.transform(c.crs,raster_proj.ExportToProj4(),
+                [lon],[lat])
+            repr_p= Point(pt[0][0],pt[1][0])
+        repr_zone=transform.transform_geom(c.crs,
+                raster_proj.ExportToProj4(),
+                zone[1]['geometry'])
+        point_val = point_query(repr_p, rasters[keys[k]])
+        zs =  zonal_stats([repr_zone], rasters[keys[k]],stats=['min', 'max', 'mean', 'median','count','unique'])
+        zhist= zonal_stats([repr_zone],
+                rasters[keys[k]],
+                categorical=True)[0]
+        k1=np.fromiter(zhist.keys(),dtype='int')
+        v1=np.fromiter(zhist.values(),dtype='int')
+        bin_edges = None 
+        if bins is not None and k1.size>0:
+            if (bins<k1.size) :
+                seq = np.arange(zs[0]['min'],zs[0]['max']+1,1)
+                bin_edges=np.histogram_bin_edges(seq,bins=bins).astype('int')
+        hist = to_dict(k1,v1,bin_edges,percent)
+        res ={'Origin'   : point_val[0],
+            'Min'    : zs[0]['min'],
+            'Max'    : zs[0]['max'],
+            'Mean'   : zs[0]['mean'],
+            'Median' : zs[0]['median'],
+            'Count' : zs[0]['count'],
+            'Hist' : hist
+            }
+        zone[1]['properties'][keys[k]] = res
+    results.append(zone[1]['properties'])
+
+results2= {"type":"FeatureCollection","features": results}
+print ('Content-Type: application/json\n');
+print(json.dumps(results2,default=myconverter))
+c.close()
