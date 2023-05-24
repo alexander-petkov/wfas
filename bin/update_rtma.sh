f78ba8d43a1030456cde8017eb8b87675c9f20f4
alexander-petkov <greenkov@gmail.com>
2023-05-24 15:04:41 +0000

    Added retention period  and netcdf package export statement to update_rtma.sh

--- bin/update_rtma.sh
+++ bin/update_rtma.sh
@@ -20,11 +20,15 @@ EXTRACT_FROM=('varanl' 'varanl' 'pcp' 'varanl' 'varanl' 'varanl' 'varanl')
 BAND=(3 13 1 9 8 0 0)
 PROJ4_SRS='+proj=lcc +lat_0=25 +lon_0=-95 +lat_1=25 +lat_2=25 +x_0=0 +y_0=0 +R=6371200 +units=m +no_defs'
 
+RETENTION_PERIOD="6 weeks"
+
 ELEV_FILE=${RTMA_DIR}/${WORKSPACE}_dem.tif
 
 get_remote_files
+#printf '%s\n' ${REMOTE_FILES[@]}
 download_data
 process_new_granules
 remove_old_granules
 clean_up
+${MOUNT_DIR}/wfas/bin/netcdf_package_export.sh archive=rtma starttime="`date +'%Y-%m-%dT00:00:00Z' -d '-1 day'`"
 exit 0
