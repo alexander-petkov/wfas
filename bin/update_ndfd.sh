49109ed3bd1359809275a4cabec1dea43ff312a6
alexander-petkov <greenkov@gmail.com>
2024-12-20 11:18:11 +0000

    Minor edits to update_ndfd.sh

--- bin/update_ndfd.sh
+++ bin/update_ndfd.sh
@@ -12,7 +12,7 @@ NDFD_DIR=${DATA_DIR}/ndfd
 
 VARS=('temp' 'rhm' 'qpf' 'wspd' 'wdir' 'sky' 'pop12' 'solar')
 REMOTE_DIR=('VP.001-003' 'VP.004-007')
-REMOTE_DIR=('VP.001-003')
+#REMOTE_DIR=('VP.001-003')
 PROJ4_SRS='+proj=lcc +lat_0=25 +lon_0=-95 +lat_1=25 +lat_2=25 +x_0=0 +y_0=0 +R=6371200 +units=m +no_defs'
 
 #Build temporal filter:
@@ -63,7 +63,7 @@ function compute_solar {
 	      ${ELEV_FILE} ${NDFD_DIR}/${1}/tif/${filename}.asc
       gdal_translate -q -ot Int16 -of GTiff ${GEOTIFF_OPTIONS} \
 	      ${NDFD_DIR}/${1}/tif/${filename}.asc ${NDFD_DIR}/${1}/tif/${filename}.tif
-      rm ${NDFD_DIR}/${1}/tif/${filename}.{asc,prj}
+      rm ${NDFD_DIR}/${1}/tif/${filename}.{asc,prj,tif.aux.xml}
    done
 }
 
